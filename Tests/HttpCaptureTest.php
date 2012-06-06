ce12bfadd6174f66688ce6e60c23482303fea409
Tim Hemming <timhemming@gmail.com>
2012-06-06 22:45:39 +0100

    Setup unit testing with dependencies. Also setup travis CI project file.

---
+++ Tests/HttpCaptureTest.php
@@ -0,0 +1,54 @@
+<?php
+namespace Pequin\HttpCaptureBundle\Tests;
+
+use Pequin\HttpCaptureBundle\HttpCapture;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Monolog\Logger;
+use Monolog\Handler\TestHandler;
+
+class HttpCaptureTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDisable()
+    {
+        $handler = new TestHandler();
+        $logger = new Logger('unittest');
+        $logger->pushHandler($handler);
+
+        $request = new Request(
+            array('varOne' => 'one')
+
+        );
+
+        $response = new Response(
+            'This is the result content',
+            200,
+            array(
+                'Content-Type' => 'text/plain',
+            )
+        );
+
+        $kernel = $this->getMock('\Symfony\Component\HttpKernel\HttpKernelInterface');
+        $event = new FilterResponseEvent($kernel, $request, HttpKernelInterface::MASTER_REQUEST, $response);
+
+        $capture = new HttpCapture($logger, true);
+        $capture->setMaxLength(2000);
+        $capture->onKernelResponse($event);
+
+        $records = $handler->getRecords();
+
+        $this->assertEquals('http_capture', $records[0]['context'][0]);
+        $this->assertEquals('request', $records[0]['context'][1]);
+        $this->assertContains('Request content is empty', $records[0]['message']);
+        $this->assertEquals(\Monolog\Logger::INFO, $records[0]['level']);
+
+        $this->assertEquals('http_capture', $records[1]['context'][0]);
+        $this->assertEquals('response', $records[1]['context'][1]);
+        $this->assertContains('This is the result content', $records[1]['message']);
+        $this->assertContains('content-type: text/plain', $records[1]['message'], true);
+        $this->assertEquals(\Monolog\Logger::INFO, $records[1]['level']);
+    }
+}
